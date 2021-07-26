0c13377361b246fe8f2e621ad4c5b2e50bdc0dd3
Lauren Taylor <56175207+mrslaurentaylor@users.noreply.github.com>
2021-07-26 17:53:07 +0000

    Made changes to main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from turtle import Turtle, Screen
 t: Turtle = Turtle() # you don't have to do the : Turtle part, but that is just a guess as to what the type is, python automatically selects your type based on your program
 s: Screen = Screen()
 
+# Draw square
 t.forward(100)
 t.right(90)
 t.forward(100)

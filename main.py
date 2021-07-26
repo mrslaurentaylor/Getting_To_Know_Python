169ac4c9547049f395f29b469edc7b1353f8239e
laurenmtaylor <>
2021-07-26 17:44:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+#Creating Turtle and Screen
+t: Turtle = Turtle() # you don't have to do the : Turtle part, but that is just a guess as to what the type is, python automatically selects your type based on your program
+s: Screen = Screen()
+
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
